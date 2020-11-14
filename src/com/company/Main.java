7be3802a01a8bd1bb7927fb759e5a859bc2115d6
Selçuk Çalışkan <45595456+SelcukCaliskan@users.noreply.github.com>
2020-11-14 09:34:16 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Git'den Merhaba");
+                System.out.println("Githup'dan Merhaba");
+
     }
 }
